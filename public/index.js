641be0c4e44466bd6ab0ed83f5e1d700df81cf18
Evgenii <evgenypetrov5230@gmail.com>
2023-01-21 14:30:35 +0300

    feat: infinite scroll

--- public/index.js
+++ public/index.js
@@ -35,94 +35,156 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
-var input = document.querySelector('input'), button = document.querySelector('button'), outputBlock = document.querySelector('.hero__cards'), quantityBlock = document.querySelector('.hero__quantity');
-var template = function (items) {
-    items.map(function (item) {
-        if (outputBlock) {
-            return outputBlock.insertAdjacentHTML('beforeend', "\n\t\t\t\t<div class=\"hero__card card\">\n\t\t\t\t<img src=\"../src/assets/folder.svg\" alt=\"folder image\">\n\t\t\t\t\t<div class=\"card__info\">\n\t\t\t\t\t\t<a target=\"_blank\" href=\"".concat(item.clone_url, "\" class=\"card__link\">\n\t\t\t\t\t\t").concat(item.full_name, "\n\t\t\t\t\t\t</a>\n\t\t\t\t\t\t<p class=\"card__description\">\n\t\t\t\t\t\t").concat(item.description ? item.description : 'Without description', "\n\t\t\t\t\t\t</p>\n\t\t\t\t\t</div>\n\t\t\t\t</div>\n\t\t\t"));
+(function () {
+    var _this = this;
+    // Globals
+    var input = document.querySelector('input'), button = document.querySelector('button'), outputBlock = document.querySelector('.hero__cards'), quantityBlock = document.querySelector('.hero__quantity'), loader = document.querySelector('.loader');
+    // Basic Logic
+    var template = function (items) {
+        items.map(function (item) {
+            if (outputBlock) {
+                return outputBlock.insertAdjacentHTML('beforeend', "\n\t\t\t\t<div class=\"hero__card card\">\n\t\t\t\t<img src=\"../src/assets/folder.svg\" alt=\"folder image\">\n\t\t\t\t\t<div class=\"card__info\">\n\t\t\t\t\t\t<a target=\"_blank\" href=\"".concat(item.clone_url, "\" class=\"card__link\">\n\t\t\t\t\t\t").concat(item.full_name, "\n\t\t\t\t\t\t</a>\n\t\t\t\t\t\t<p class=\"card__description\">\n\t\t\t\t\t\t").concat(item.description ? item.description : 'Without description', "\n\t\t\t\t\t\t</p>\n\t\t\t\t\t</div>\n\t\t\t\t</div>\n\t\t\t"));
+            }
+        });
+    };
+    var renderEmptyResults = function () {
+        if (quantityBlock) {
+            quantityBlock.textContent = 'По вашему запросу ничего не найдено';
+        }
+    };
+    var renderCount = function (totalCount) {
+        if (totalCount === 0) {
+            renderEmptyResults();
+            return;
+        }
+        if (typeof totalCount === 'number' && quantityBlock) {
+            quantityBlock.textContent = "\u041D\u0430\u0439\u0434\u0435\u043D\u043E ".concat(totalCount, " \u0440\u0435\u0437\u0443\u043B\u044C\u0442\u0430\u0442\u043E\u0432");
+        }
+    };
+    var clearing = function () {
+        if (quantityBlock && outputBlock) {
+            quantityBlock.textContent = outputBlock.innerHTML = '';
+        }
+    };
+    var disableSearch = function (isDisabled) {
+        if (button && input) {
+            button.disabled = input.disabled = isDisabled;
+        }
+    };
+    var onSubmitStart = function () {
+        if (quantityBlock) {
+            quantityBlock.textContent = 'Загрузка...';
+        }
+    };
+    var renderError = function (message) {
+        if (quantityBlock) {
+            quantityBlock.textContent = "\u041F\u0440\u043E\u0438\u0437\u043E\u0448\u043B\u0430 \u043E\u0448\u0438\u0431\u043A\u0430.\n".concat(message);
+        }
+    };
+    // Async Logic
+    var API_URL = 'https://api.nomoreparties.co/github-search?per_page=15&sort=stars&q=';
+    var search = '';
+    var limit = 0;
+    var getData = function (search, page) {
+        if (page === void 0) { page = 1; }
+        return __awaiter(_this, void 0, void 0, function () {
+            var res, error_1;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        _a.trys.push([0, 3, 4, 5]);
+                        return [4 /*yield*/, fetch(API_URL + search + "&page=".concat(page))];
+                    case 1:
+                        res = _a.sent();
+                        return [4 /*yield*/, res.json()];
+                    case 2: return [2 /*return*/, _a.sent()];
+                    case 3:
+                        error_1 = _a.sent();
+                        error_1 instanceof Error && renderError(error_1.message);
+                        return [2 /*return*/, { total_count: null, items: [] }];
+                    case 4:
+                        disableSearch(false);
+                        return [7 /*endfinally*/];
+                    case 5: return [2 /*return*/];
+                }
+            });
+        });
+    };
+    var onSubmit = function () { return __awaiter(_this, void 0, void 0, function () {
+        var value, _a, total_count, items, lastCard;
+        return __generator(this, function (_b) {
+            switch (_b.label) {
+                case 0:
+                    value = (input === null || input === void 0 ? void 0 : input.value) || '';
+                    if (!value.trim()) return [3 /*break*/, 2];
+                    search = value;
+                    disableSearch(true);
+                    clearing();
+                    onSubmitStart();
+                    return [4 /*yield*/, getData(value)];
+                case 1:
+                    _a = _b.sent(), total_count = _a.total_count, items = _a.items;
+                    renderCount(total_count);
+                    items && template(items);
+                    if (total_count) {
+                        limit = Math.ceil(total_count / 15);
+                    }
+                    lastCard = document.querySelector('.hero__card:last-child');
+                    if (lastCard) {
+                        infiniteObserver.observe(lastCard);
+                    }
+                    _b.label = 2;
+                case 2: return [2 /*return*/];
+            }
+        });
+    }); };
+    // Attach Events
+    button === null || button === void 0 ? void 0 : button.addEventListener('click', onSubmit);
+    input === null || input === void 0 ? void 0 : input.addEventListener('keypress', function (event) {
+        if (event.code === 'Enter') {
+            onSubmit();
         }
     });
-};
-var renderEmptyResults = function () {
-    if (quantityBlock) {
-        quantityBlock.textContent = 'По вашему запросу ничего не найдено';
-    }
-};
-var renderCount = function (totalCount) {
-    if (totalCount === 0) {
-        renderEmptyResults();
-        return;
-    }
-    if (typeof totalCount === 'number' && quantityBlock) {
-        quantityBlock.textContent = "\u041D\u0430\u0439\u0434\u0435\u043D\u043E ".concat(totalCount, " \u0440\u0435\u0437\u0443\u043B\u044C\u0442\u0430\u0442\u043E\u0432");
-    }
-};
-var clearing = function () {
-    if (quantityBlock && outputBlock) {
-        quantityBlock.textContent = outputBlock.innerHTML = '';
-    }
-};
-var disableSearch = function (isDisabled) {
-    if (button && input) {
-        button.disabled = input.disabled = isDisabled;
-    }
-};
-var onSubmitStart = function () {
-    if (quantityBlock) {
-        quantityBlock.textContent = 'Загрузка...';
-    }
-};
-var renderError = function (message) {
-    if (quantityBlock) {
-        quantityBlock.textContent = "\u041F\u0440\u043E\u0438\u0437\u043E\u0448\u043B\u0430 \u043E\u0448\u0438\u0431\u043A\u0430.\n".concat(message);
-    }
-};
-var API_URL = 'https://api.nomoreparties.co/github-search?per_page=15&sort=stars&q=';
-var getData = function (url) { return __awaiter(void 0, void 0, void 0, function () {
-    var res, error_1;
-    return __generator(this, function (_a) {
-        switch (_a.label) {
-            case 0:
-                _a.trys.push([0, 3, 4, 5]);
-                return [4 /*yield*/, fetch(url)];
-            case 1:
-                res = _a.sent();
-                return [4 /*yield*/, res.json()];
-            case 2: return [2 /*return*/, _a.sent()];
-            case 3:
-                error_1 = _a.sent();
-                error_1 instanceof Error && renderError(error_1.message);
-                return [2 /*return*/, { total_count: null, items: [] }];
-            case 4:
-                disableSearch(false);
-                return [7 /*endfinally*/];
-            case 5: return [2 /*return*/];
+    // Observer Logic
+    var nextPage = 2;
+    var infiniteObserver = new IntersectionObserver(function (_a, observer) {
+        var entry = _a[0];
+        if (entry.isIntersecting) {
+            observer.unobserve(entry.target);
+            if (nextPage <= limit) {
+                loadOnScroll(nextPage++);
+            }
         }
-    });
-}); };
-var onSubmit = function () { return __awaiter(void 0, void 0, void 0, function () {
-    var search, _a, total_count, items;
-    return __generator(this, function (_b) {
-        switch (_b.label) {
-            case 0:
-                search = (input === null || input === void 0 ? void 0 : input.value) || '';
-                if (!search.trim()) return [3 /*break*/, 2];
-                disableSearch(true);
-                clearing();
-                onSubmitStart();
-                return [4 /*yield*/, getData(API_URL + search)];
-            case 1:
-                _a = _b.sent(), total_count = _a.total_count, items = _a.items;
-                renderCount(total_count);
-                items && template(items);
-                _b.label = 2;
-            case 2: return [2 /*return*/];
-        }
-    });
-}); };
-button === null || button === void 0 ? void 0 : button.addEventListener('click', onSubmit);
-input === null || input === void 0 ? void 0 : input.addEventListener('keypress', function (event) {
-    if (event.code === 'Enter') {
-        onSubmit();
-    }
-});
+    }, {});
+    var loadOnScroll = function (page) { return __awaiter(_this, void 0, void 0, function () {
+        var res, items, lastCard, error_2;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    if (!search) return [3 /*break*/, 5];
+                    _a.label = 1;
+                case 1:
+                    _a.trys.push([1, 4, , 5]);
+                    loader && loader.classList.remove('hidden');
+                    return [4 /*yield*/, fetch(API_URL + search + "&page=".concat(page))];
+                case 2:
+                    res = _a.sent();
+                    return [4 /*yield*/, res.json()];
+                case 3:
+                    items = (_a.sent()).items;
+                    loader && loader.classList.add('hidden');
+                    items && template(items);
+                    lastCard = document.querySelector('.hero__card:last-child');
+                    if (lastCard) {
+                        infiniteObserver.observe(lastCard);
+                    }
+                    return [3 /*break*/, 5];
+                case 4:
+                    error_2 = _a.sent();
+                    error_2 instanceof Error && console.log(error_2);
+                    return [3 /*break*/, 5];
+                case 5: return [2 /*return*/];
+            }
+        });
+    }); };
+})();
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,22 @@
-interface Item {
-	full_name: string;
-	description: string | null;
-	clone_url: string;
-}
+(function() {
+	// Globals
+	const input = document.querySelector('input'),
+		button = document.querySelector('button'),
+		outputBlock = document.querySelector('.hero__cards'),
+		quantityBlock = document.querySelector('.hero__quantity'),
+		loader = document.querySelector('.loader');
+	
+	interface Item {
+		full_name: string;
+		description: string | null;
+		clone_url: string;
+	}
 
-const input = document.querySelector('input'),
-	button = document.querySelector('button'),
-	outputBlock = document.querySelector('.hero__cards'),
-	quantityBlock = document.querySelector('.hero__quantity');
-
-const template = (items: Item[]) => {
-	items.map(item => {
-		if (outputBlock) {
-			return outputBlock.insertAdjacentHTML('beforeend', `
+// Basic Logic
+	const template = (items: Item[]) => {
+		items.map(item => {
+			if (outputBlock) {
+				return outputBlock.insertAdjacentHTML('beforeend', `
 				<div class="hero__card card">
 				<img src="../src/assets/folder.svg" alt="folder image">
 					<div class="card__info">
@@ -25,83 +29,131 @@ const template = (items: Item[]) => {
 					</div>
 				</div>
 			`);
+			}
+		});
+	};
+	
+	const renderEmptyResults = () => {
+		if (quantityBlock) {
+			quantityBlock.textContent = 'По вашему запросу ничего не найдено';
+		}
+	};
+	
+	const renderCount = (totalCount: number | null) => {
+		if (totalCount === 0) {
+			renderEmptyResults();
+			return;
+		}
+		
+		if (typeof totalCount === 'number' && quantityBlock) {
+			quantityBlock.textContent = `Найдено ${totalCount} результатов`;
+		}
+	};
+	
+	const clearing = () => {
+		if (quantityBlock && outputBlock) {
+			quantityBlock.textContent = outputBlock.innerHTML = '';
+		}
+	};
+	
+	const disableSearch = (isDisabled: boolean) => {
+		if (button && input) {
+			button.disabled = input.disabled = isDisabled;
+		}
+	};
+	
+	const onSubmitStart = () => {
+		if (quantityBlock) {
+			quantityBlock.textContent = 'Загрузка...';
+		}
+	};
+	
+	const renderError = (message: string) => {
+		if (quantityBlock) {
+			quantityBlock.textContent = `Произошла ошибка.\n${message}`;
+		}
+	};
+
+// Async Logic
+	const API_URL = 'https://api.nomoreparties.co/github-search?per_page=15&sort=stars&q=';
+	let search = '';
+	let limit = 0;
+	
+	const getData = async (search: string, page = 1): Promise<{ total_count: number | null, items: Item[] }> => {
+		try {
+			const res = await fetch(API_URL + search + `&page=${page}`);
+			return await res.json();
+		} catch (error) {
+			error instanceof Error && renderError(error.message);
+			return { total_count: null, items: [] };
+		} finally {
+			disableSearch(false);
+		}
+	};
+	
+	const onSubmit = async () => {
+		const value = input?.value || '';
+		
+		if (value.trim()) {
+			search = value;
+			disableSearch(true);
+			clearing();
+			onSubmitStart();
+			
+			const { total_count, items } = await getData(value);
+			renderCount(total_count);
+			items && template(items);
+			
+			if (total_count) {
+				limit = Math.ceil(total_count / 15);
+			}
+			
+			const lastCard = document.querySelector('.hero__card:last-child');
+			if (lastCard) {
+				infiniteObserver.observe(lastCard);
+			}
+		}
+	};
+
+// Attach Events
+	button?.addEventListener('click', onSubmit);
+	
+	input?.addEventListener('keypress', event => {
+		if (event.code === 'Enter') {
+			onSubmit();
 		}
 	});
-};
 
-const renderEmptyResults = () => {
-	if (quantityBlock) {
-		quantityBlock.textContent = 'По вашему запросу ничего не найдено';
-	}
-};
-
-const renderCount = (totalCount: number | null) => {
-	if (totalCount === 0) {
-		renderEmptyResults();
-		return;
-	}
+// Observer Logic
+	let nextPage = 2;
 	
-	if (typeof totalCount === 'number' && quantityBlock) {
-		quantityBlock.textContent = `Найдено ${totalCount} результатов`;
-	}
-};
-
-const clearing = () => {
-	if (quantityBlock && outputBlock) {
-		quantityBlock.textContent = outputBlock.innerHTML = '';
-	}
-};
-
-const disableSearch = (isDisabled: boolean) => {
-	if (button && input) {
-		button.disabled = input.disabled = isDisabled;
-	}
-};
-
-const onSubmitStart = () => {
-	if (quantityBlock) {
-		quantityBlock.textContent = 'Загрузка...';
-	}
-};
-
-const renderError = (message: string) => {
-	if (quantityBlock) {
-		quantityBlock.textContent = `Произошла ошибка.\n${message}`;
-	}
-};
-
-const API_URL = 'https://api.nomoreparties.co/github-search?per_page=15&sort=stars&q=';
-
-const getData = async (url: string): Promise<{ total_count: number | null, items: Item[] }> => {
-	try {
-		const res = await fetch(url);
-		return await res.json();
-	} catch (error) {
-		error instanceof Error && renderError(error.message);
-		return { total_count: null, items: [] };
-	} finally {
-		disableSearch(false);
-	}
-};
-
-const onSubmit = async () => {
-	const search = input?.value || '';
+	const infiniteObserver = new IntersectionObserver(
+		([entry], observer) => {
+			if (entry.isIntersecting) {
+				observer.unobserve(entry.target);
+				if (nextPage <= limit) {
+					loadOnScroll(nextPage++);
+				}
+			}
+		}, {});
 	
-	if (search.trim()) {
-		disableSearch(true);
-		clearing();
-		onSubmitStart();
-		
-		const { total_count, items } = await getData(API_URL + search);
-		renderCount(total_count);
-		items && template(items);
-	}
-};
-
-button?.addEventListener('click', onSubmit);
-
-input?.addEventListener('keypress', event => {
-	if (event.code === 'Enter') {
-		onSubmit();
-	}
-});
+	const loadOnScroll = async (page: number) => {
+		if (search) {
+			try {
+				loader && loader.classList.remove('hidden');
+				const res = await fetch(API_URL + search + `&page=${page}`);
+				const { items } = await res.json();
+				loader && loader.classList.add('hidden');
+				items && template(items);
+				
+				const lastCard = document.querySelector('.hero__card:last-child');
+				if (lastCard) {
+					infiniteObserver.observe(lastCard);
+				}
+			} catch (error) {
+				error instanceof Error && console.log(error);
+			}
+		}
+	};
+	
+})();
